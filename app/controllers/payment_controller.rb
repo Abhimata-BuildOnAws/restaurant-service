684c79ce05335e782fb70edf7179df7d6009d83f
bingyuyap <bingyu.yap.21@gmail.com>
2021-07-11 21:47:57 +0800

    Update params for payment_intent methods

--- app/controllers/payment_controller.rb
+++ app/controllers/payment_controller.rb
@@ -2,6 +2,7 @@ class PaymentController < ApplicationController
   require 'stripe'
   Stripe.api_key = ENV['STRIPE_SECRET_KEY']
 
+  # Takes in amount to be paid and the account to be paid to
   def payment_intent
     payment_intent = Stripe::PaymentIntent.create({
       payment_method_types: ['card'],
@@ -14,9 +15,11 @@ class PaymentController < ApplicationController
     render :json => { client_secret: payment_intent.client_secret }, status: 200
   end
 
+  # Takes in total cost and composition of the cost to be paid to restaurant and delivery respectively
+  # WIP : Could be calculatedk waiting for Jon
   def payment_intent_group
     payment_intent = Stripe::PaymentIntent.create({
-      amount: 10000,
+      amount: params[:total_cost],
       currency: 'sgd',
       payment_method_types: ['card'],
       transfer_group: params[:order_id],
@@ -24,7 +27,7 @@ class PaymentController < ApplicationController
     
     # Create a Transfer to a connected account (later):
     transfer = Stripe::Transfer.create({
-      amount: 7000,
+      amount: params[:food_cost],
       currency: 'sgd',
       destination: params[:restaurant_id],
       transfer_group: params[:order_id],
@@ -32,7 +35,7 @@ class PaymentController < ApplicationController
     
     # Create a second Transfer to another connected account (later):
     transfer = Stripe::Transfer.create({
-      amount: 2000,
+      amount: params[:delivery_cost],
       currency: 'sgd',
       destination: params[:deliverer_id],
       transfer_group: params[:order_id],
