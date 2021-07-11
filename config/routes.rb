d30d210a5af6a5484624c362a29322dc66fcf1e3
bingyuyap <bingyu.yap.21@gmail.com>
2021-07-11 17:01:29 +0800

    Clean up account routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@
 
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # Account
   post '/signup', to: 'account#signup'
   post '/login', to: 'account#login'
   post '/restaurant/signup', to: 'seller#signup'
