746d208cd33114a8915096cfe89b3484383e59e9
bingyuyap <bingyu.yap.21@gmail.com>
2021-07-11 21:41:08 +0800

    Add payment intent group method

--- app/controllers/payment_controller.rb
+++ app/controllers/payment_controller.rb
@@ -13,4 +13,29 @@ class PaymentController < ApplicationController
     })
     render :json => { client_secret: payment_intent.client_secret }, status: 200
   end
+
+  def payment_intent_group
+    payment_intent = Stripe::PaymentIntent.create({
+      amount: 10000,
+      currency: 'sgd',
+      payment_method_types: ['card'],
+      transfer_group: params[:order_id],
+    })
+    
+    # Create a Transfer to a connected account (later):
+    transfer = Stripe::Transfer.create({
+      amount: 7000,
+      currency: 'sgd',
+      destination: params[:restaurant_id],
+      transfer_group: params[:order_id],
+    })
+    
+    # Create a second Transfer to another connected account (later):
+    transfer = Stripe::Transfer.create({
+      amount: 2000,
+      currency: 'sgd',
+      destination: params[:deliverer_id],
+      transfer_group: params[:order_id],
+    })
+  end
 end
