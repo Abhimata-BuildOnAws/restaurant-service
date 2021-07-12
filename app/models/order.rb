996fba26fc5229b59b33f3bc4e8302a22df8c635
bingyuyap <bingyu.yap.21@gmail.com>
2021-07-12 19:06:54 +0800

    Change belong_to to has_one

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,5 +2,6 @@ class Order < ApplicationRecord
   has_many :order_items
   has_many :menu_items, through: :order_items
 
-  belongs_to :hitch
+  has_one :hitch_order
+  has_one :hitch, through: :hitch_order
 end
