406da68dd598bcc8088d96ff5b2c8ec0a1bb947b
bingyuyap <bingyu.yap.21@gmail.com>
2021-07-12 10:03:10 +0800

    Remove payment_intent_group

--- app/controllers/payment_controller.rb
+++ app/controllers/payment_controller.rb
@@ -14,31 +14,4 @@ class PaymentController < ApplicationController
     })
     render :json => { client_secret: payment_intent.client_secret }, status: 200
   end
-
-  # Takes in total cost and composition of the cost to be paid to restaurant and delivery respectively
-  # WIP : Could be calculatedk waiting for Jon
-  def payment_intent_group
-    payment_intent = Stripe::PaymentIntent.create({
-      amount: params[:total_cost],
-      currency: 'sgd',
-      payment_method_types: ['card'],
-      transfer_group: params[:order_id],
-    })
-    
-    # Create a Transfer to a connected account (later):
-    transfer = Stripe::Transfer.create({
-      amount: params[:food_cost],
-      currency: 'sgd',
-      destination: params[:restaurant_id],
-      transfer_group: params[:order_id],
-    })
-    
-    # Create a second Transfer to another connected account (later):
-    transfer = Stripe::Transfer.create({
-      amount: params[:delivery_cost],
-      currency: 'sgd',
-      destination: params[:deliverer_id],
-      transfer_group: params[:order_id],
-    })
-  end
 end
