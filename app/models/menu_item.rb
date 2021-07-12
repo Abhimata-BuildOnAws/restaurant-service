b561fef06ecd13adaa16663ae9ee92688d491e00
bingyuyap <bingyu.yap.21@gmail.com>
2021-07-12 14:59:23 +0800

    Add belongs_to relationship to menu items

--- app/models/menu_item.rb
+++ app/models/menu_item.rb
@@ -1,6 +1,7 @@
 class MenuItem < ApplicationRecord
   # Relationships
   belongs_to :restaurant
+  belongs_to :order
 
   # Validations
   validates_uniqueness_of :name, scope: %i[restaurant_id], message: 'This item is already listed under your restaurant'
