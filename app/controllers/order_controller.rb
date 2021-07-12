2a94dc103b80bcac8f2e68e1fca645012553eef3
bingyuyap <bingyu.yap.21@gmail.com>
2021-07-12 16:53:09 +0800

    Add total price to orders

--- app/controllers/order_controller.rb
+++ app/controllers/order_controller.rb
@@ -4,12 +4,22 @@ class OrderController < ApplicationController
     new_order = Order.create()
     orders = JSON.parse(request.body.read)['order']
 
+    total_price = 0
+
     orders.each do |o|
-      menu_item_id = o['menu_item_id']
+      menu_item = find_menu_item(o['menu_item_id'])
       quantity = o['quantity']
-      OrderItem.create(order_id: new_order.id, quantity: quantity, menu_item_id: menu_item_id)
+      OrderItem.create(order_id: new_order.id, quantity: quantity, menu_item_id: menu_item.id)
+      total_price += quantity * menu_item.price
     end
 
-    render :json => { order: new_order }, status: 200
+    new_order.update(total_price: total_price)
+
+    render :json => { order: new_order}, status: 200
+  end
+
+  private 
+  def find_menu_item(menu_item_id)
+    MenuItem.find_by(id: menu_item_id)
   end
 end
