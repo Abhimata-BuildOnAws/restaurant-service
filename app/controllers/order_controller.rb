08d8a66df57b4bb98749bb7d3832cbb942087621
bingyuyap <bingyu.yap.21@gmail.com>
2021-07-12 16:27:03 +0800

    Add OrderController

--- app/controllers/order_controller.rb
+++ app/controllers/order_controller.rb
@@ -1,2 +1,15 @@
 class OrderController < ApplicationController
+  require 'json'
+  def create
+    new_order = Order.create()
+    orders = JSON.parse(request.body.read)['order']
+
+    orders.each do |o|
+      menu_item_id = o['menu_item_id']
+      quantity = o['quantity']
+      OrderItem.create(order: new_order.id, quantity: quantity, menu_item_id: menu_item_id)
+    end
+
+    render :json => { order: order }, status: 200
+  end
 end
