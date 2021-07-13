a61960e05d8acb4519a1170cf553f2348cae7297
Gary Peabody <aikarvin99@gmail.com>
2021-07-13 15:11:55 +0800

    Retrieve currently logged in user

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -71,16 +71,32 @@ class AuthController < ApplicationController
     end
   end
 
-  def get_user
+  def current_user
     begin
       resp = Cognito.get_user(params[:access_token])
-      render json: {
-        user_id: resp.username,
-        user_attributes: resp.user_attributes
-      },status: 200
+
+      #Initialise variables
+      user_type = user_id = ''
+      
+      # Determine user_type and id of currently logged in user
+      resp.user_attributes.each do |t|
+        if t['name'] == 'sub'
+          user_id = t['value']
+        elsif t['name'] == 'custom:User_Type'
+          user_type = t['value']
+        end
+      end
     rescue => e
       render json: e
     end
+
+    case user_type
+    when 'restaurant'
+      user = Restaurant.find(user_id)
+    when 'user'
+      user = User.find(user_id)
+    end
+    render json: user
   end
 
   # Challenges
