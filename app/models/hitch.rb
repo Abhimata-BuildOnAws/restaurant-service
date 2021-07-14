d56186cb9bdba100ea380fbfd1f2433510e4c877
bingyuyap <bingyu.yap.21@gmail.com>
2021-07-14 10:53:10 +0800

    Update order-hitch relationship

--- app/models/hitch.rb
+++ app/models/hitch.rb
@@ -1,6 +1,5 @@
 class Hitch < ApplicationRecord
-  has_many :hitch_orders
-  has_many :orders, through: :hitch_orders
+  has_many :orders
 
   # Calculate pollution each customer emitted if they were to hitch on a deliver
   # WIP : change 100 to pollution index
