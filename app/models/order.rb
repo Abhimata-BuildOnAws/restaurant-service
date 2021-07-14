e4f935104166b94c88a34f6a255d54c6216afe4a
bingyuyap <bingyu.yap.21@gmail.com>
2021-07-14 10:52:53 +0800

    Update order-hitch relationship

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,6 +2,5 @@ class Order < ApplicationRecord
   has_many :order_items
   has_many :menu_items, through: :order_items
 
-  has_one :hitch_order
-  has_one :hitch, through: :hitch_order
+  has_one :hitch
 end
