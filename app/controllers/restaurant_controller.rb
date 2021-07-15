69369539b16deb57388070915e60e7c0210b1bac
bingyuyap <bingyu.yap.21@gmail.com>
2021-07-16 07:33:42 +0800

    Update restaurant controller to serialize restaurants and add current user method

--- app/controllers/restaurant_controller.rb
+++ app/controllers/restaurant_controller.rb
@@ -1,6 +1,38 @@
 class RestaurantController < ApplicationController
   def browse
-    restaurants = ::Restaurant.all
-    render json: { restaurants: restaurants }, status: 200
+    restaurant = ::Restaurant.all
+    
+    serializer = RestaurantSerializer.new(restaurant, {params: {current_user: current_user}})
+    render json: serializer.serializable_hash
+  end
+
+
+  private
+  def current_user
+    begin
+      resp = Cognito.get_user(params[:access_token])
+
+      #Initialise variables
+      user_type = user_id = ''
+      
+      # Determine user_type and id of currently logged in user
+      resp.user_attributes.each do |t|
+        if t['name'] == 'sub'
+          user_id = t['value']
+        elsif t['name'] == 'custom:User_Type'
+          user_type = t['value']
+        end
+      end
+    rescue => e
+      render json: e
+    end
+
+    case user_type
+    when 'restaurant'
+      user = Restaurant.find(user_id)
+    when 'user'
+      user = User.find(user_id)
+    end
+    user
   end
 end
