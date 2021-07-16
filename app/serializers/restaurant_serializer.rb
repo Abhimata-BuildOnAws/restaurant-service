1da7f9bdc78c9a89f8321006b715a86a601a7480
bingyuyap <bingyu.yap.21@gmail.com>
2021-07-16 11:18:17 +0800

    Add all other attributes for restaurant_serializer

--- app/serializers/restaurant_serializer.rb
+++ app/serializers/restaurant_serializer.rb
@@ -1,12 +1,8 @@
 class RestaurantSerializer
   include JSONAPI::Serializer
 
-  attributes :id, :name, :latitude, :longitude #, :street, :state, :country, :contact_no, :stripe_id,
-  # belongs_to :user
-  # belongs_to :tumpang
+  attributes :id, :name, :latitude, :longitude, :street, :state, :country, :contact_no, :stripe_id
 
-
-  # WIP : seed data for lat and long for user and restaurant 
   attribute :absolute_distance do |restaurant, params|
     current_user = params[:current_user]
     restaurant.distance_to([current_user.latitude,current_user.longitude])
