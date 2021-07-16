b9e33ca6af5454462d2ddc16d2c3ba1f59553491
bingyuyap <bingyu.yap.21@gmail.com>
2021-07-16 13:57:38 +0800

    Add comments to hitch serializer

--- app/serializers/hitch_serializer.rb
+++ app/serializers/hitch_serializer.rb
@@ -1,18 +1,21 @@
 class HitchSerializer
   include JSONAPI::Serializer
 
-  attributes :pickup, :absolute_distance, :latitude, :longitude
+  attributes :pickup, :latitude, :longitude
 
+  # Returns a straight distance (not accounting route distance) from restaurant to the user in metres
   attribute :absolute_distance do |hitch, params|
     current_user = params[:current_user]
     hitch.distance_to(current_user.coordinates)
   end
 
+  # Returns the route distance from restaurant to the user in metres 
   attribute :travel_distance do |hitch, params|
     current_user = params[:current_user]
     @distance = get_distance(hitch, current_user)
   end
 
+  # Returns the total grams of carbon emitted for the whole delivery in grams
   attribute :total_pollution do |hitch, params|
     current_user = params[:current_user]
     # WIP : Average grams of carbon emitted per kilometer is 250g
@@ -21,6 +24,7 @@ class HitchSerializer
 
 
   # Calculates effective pollution for each customer that hitches onto this delivery
+  # In grams
   attribute :effective_pollution do |hitch, params|
     order_count = hitch.orders.count
     if order_count == 0
