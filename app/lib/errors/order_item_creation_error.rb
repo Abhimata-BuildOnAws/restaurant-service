e082f1adfb6738522f9f59005d0cb2418d8f7c66
bingyuyap <bingyu.yap.21@gmail.com>
2021-07-17 12:33:16 +0800

    Fix module name for OrderItemCreationError and call Errors::OrderItemCreationError

--- app/lib/errors/order_item_creation_error.rb
+++ app/lib/errors/order_item_creation_error.rb
@@ -1,5 +1,7 @@
-class OrderItemCreationError < StandardError
-  def message
-    'There was a problem with your order, you must order from the same restaurant as your Tumpang'
+module Errors
+  class OrderItemCreationError < StandardError
+    def message
+      'There was a problem with your order, you must order from the same restaurant as your Tumpang'
+    end
   end
 end
