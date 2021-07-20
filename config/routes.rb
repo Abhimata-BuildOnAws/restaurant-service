7fb85369966157a127eb3734842ee3d5563db77d
Gary Peabody <aikarvin99@gmail.com>
2021-07-20 17:22:52 +0800

    Bugfix routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@
 Rails.application.routes.draw do
   post '/restaurant/browse', to: 'restaurant#browse'
   post '/restaurant/create', to: 'restaurant#create_restaurant'
-  post 'restaurant/add_menu', to 'restaurant#add_menu_item_to_restaurant'
+  post 'restaurant/add_menu', to: 'restaurant#add_menu_item_to_restaurant'
 
 end
