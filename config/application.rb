89afb696cf52e400510665f2d984d3a34e7ab62f
Gary Peabody <aikarvin99@gmail.com>
2021-08-03 17:22:31 +0800

    Fix for cors

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,9 @@ module Tumpang
     config.active_record.default_timezone = :local
     config.hosts << "internal-tumpang-1967021414.ap-southeast-1.elb.amazonaws.com"
     config.hosts << "tumpang-nlb-43349993c0bf291a.elb.ap-southeast-1.amazonaws.com"
+    config.action_dispatch.default_headers = {
+      'Access-Control-Allow-Origin' => 'http://localhost:3000',
+    }
     config.before_configuration do
       env_file = File.join(Rails.root, 'config', 'local_env.yml')
       if File.exist?(env_file)
